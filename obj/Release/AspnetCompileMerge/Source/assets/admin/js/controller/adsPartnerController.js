2036359d9de0f7a1e24a02c587a3dc1dc45fe70b
lethienphu1212 <lethienphu12122002@gmail.com>
2024-01-01 15:49:01 +0700

    webxemphim

---
+++ obj/Release/AspnetCompileMerge/Source/assets/admin/js/controller/adsPartnerController.js
@@ -0,0 +1,28 @@
+﻿var adp = {
+    init: function () {
+        adp.registerEvents();
+    },
+    registerEvents: function () {
+        $('.btn-default').off('click').on('click', function (e) {
+            e.preventDefault();
+            var btn = $(this);
+            var id = $(btn).data('id');
+            $.ajax({
+                url: "/Admin/AdvertisingPartners/ChangeStatus",
+                data: { id: id },
+                dataType: "json",
+                type: "POST",
+                success: function (response) {
+                    console.log(response);
+                    if (response.status == true) {
+                        btn.text("Kích hoạt");
+                    }
+                    else {
+                        btn.text("Huỷ Kích hoạt");
+                    }
+                }
+            });
+        });
+    }
+}
+adp.init();
